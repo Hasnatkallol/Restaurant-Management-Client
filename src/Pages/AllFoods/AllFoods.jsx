7ff909a9c9db5cb4b645ceb404653d36ab0e2761
hasnat_kallol <hasnatkallol1971@gmail.com>
2025-06-13 20:09:24 +0600

    search functionality

--- src/Pages/AllFoods/AllFoods.jsx
+++ src/Pages/AllFoods/AllFoods.jsx
@@ -5,50 +5,54 @@ import AllFoodCard from "./AllFoodCard";
 
 const AllFoods = () => {
   const [foods, setFoods] = useState([]);
+  const [searchQuery, setSearchQuery] = useState("");
+  console.log(searchQuery);
 
   useEffect(() => {
-    fetch(`http://localhost:4000/foods`)
+    fetch(`http://localhost:4000/foods?search=${searchQuery}`)
       .then((res) => res.json())
       .then((data) => setFoods(data));
-  }, [setFoods]);
+  }, [searchQuery]);
   return (
     <div>
       <div
         className="hero min-h-[40vh] relative"
         style={{ backgroundImage: `url(${allFoodBg})` }}
       >
-        {/* Dark overlay for better text contrast */}
         <div className="hero-overlay bg-opacity-70 "></div>
 
         <div className="hero-content text-center text-neutral-content">
           <div className="max-w-4xl">
-       
-
-            {/* Main heading with gradient text */}
             <h1 className="mb-8 text-5xl md:text-6xl font-bold">
-              <span className=" bg-clip-text text-white">
-                All Foods
-              </span>
+              <span className=" bg-clip-text text-white">All Foods</span>
             </h1>
 
-       
-
-            {/* Decorative divider */}
             <div className="flex  justify-center items-center my-6">
               <div className="w-12  h-px bg-white mx-2"></div>
               <span className="text-white text-xl">✧</span>
               <div className="w-12 h-px bg-white mx-2"></div>
             </div>
 
-            {/* Description text */}
             <p className="mb-12 text-lg md:text-xl text-neutral-content/90">
               From savory starters to mouthwatering mains and decadent desserts
               - discover, manage, and enjoy the ultimate food experience.
             </p>
-
-        
           </div>
         </div>
+      </div>
+      <div className="w-11/12 mx-auto py-10 lg:py-15 flex flex-col items-center">
+        <input
+          type="text"
+          placeholder="Search Food"
+          className="border w-[50%] h-10 border-gray-300 shadow pl-5 rounded-xl focus:outline-none focus:border-transparent"
+          onChange={(e) => setSearchQuery(e.target.value)}
+          value={searchQuery}
+        />
+        <p className="text-center py-5">
+          Easily find your favorite dishes by typing the food name in the search
+          bar. The list updates instantly to show only the items that match your
+          search query
+        </p>
       </div>
 
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-20 w-11/12 mx-auto my-5 items-center justify-center">
